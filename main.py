8d67143b51e346498ef1b2eff20c860867c0e44e
wesley teles <wesley.teles.dev@gmail.com>
2022-10-06 17:42:16 -0300

    Alterando Caminho

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def price():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://hotmilhas.com.br/")
 
     try:
