dcb8c81d870114e32298068d9372e5b2b1837d06
wesley teles <wesley.teles.dev@gmail.com>
2022-10-26 14:40:10 -0300

    First Version

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Bot Para Realizar cotação na Hotmilhas</h1>
+
+<span>Esse bot tem como objestivo automatizar a cotação no site da hotmilhas</span>
+
+<span>Como Utilizar:</span>
+<p>Baixar o Python em sua maquina</p>
+<p>Instale as bibliotecas a partir do arquivo requirements.txt</p>
+<p>Dentro do Arquivo main.py, coloque seu e-mail no campo email = 'meuemail@.....'</p>
+<p>Escolha o Tempo de agendamento para que o bote seja execultado (Caso queira)</p>
+<p>Caso nao queria execultar com o agendamento descomente a linha 74 e comente ou apague as linhas 76 à 88</p>
+<p>Execulte o Arquivo main.py</p>
+
+<span>Version: 1.0</span>
+
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 import time
-import os
 import schedule
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,64 +8,84 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 
-mail = '//*[@id="quotation-form"]/div[1]/div[1]/input'
+# 1- Latam Pass 
+# 2- LP Platinum/Black 
+# 3- Smiles 
+# 4- Tudo Azul
+# Escolher um numero para que altera na opcao do Programa de Milhas
+opt_programa = 3
+
+# -----------------------------
+
+# 1- Smiles 
+# 2- Latam Pass 
+# 3- Tudo Azul 
+# 8- LP Platinum/Black
+# Escolher um numero para que altera na opcao do tipo do Programa de Milhas
+opt_type_prog = 1
+
+# -----------------------------
+
+# COLOQUE SEU E-MAIL ENTRE AS ASPAS ''
+email = 'meuemail@.....'
+selecte_mail = '//*[@id="quotation-form"]/div[1]/div[1]/input'
 select_program_milhas = '//*[@id="quotation-form"]/div[1]/div[2]/div'
-select_type_smiles = '//*[@id="quotation-form"]/div[1]/div[2]/div/div/div[2]/div/ul/li[3]'
-options_qtd_milhas = '//*[@id="select_points_1"]'
-select_qtd_milhas = '//*[@id="select_points_1"]/div[2]/div[2]/ul/li[81]'
+select_type_program = f'//*[@id="quotation-form"]/div[1]/div[2]/div/div/div[2]/div/ul/li[{opt_programa}]' 
+options_qtd_milhas = f'//*[@id="select_points_{opt_type_prog}"]'
+select_qtd_milhas = f'//*[@id="select_points_{opt_type_prog}"]/div[2]/div[2]/ul/li[51]'
 btn_cotar = '//*[@id="quotation-form"]/div[2]/div/div/button'
 
 def price():
+
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # Se quiser ver o bot trabalhando, comente a linha 42 ou onde tem o "--headless"
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
     driver.get("https://hotmilhas.com.br/")
 
     try:
-        insert_email = driver.find_element(by=By.XPATH, value=mail).send_keys("wesley.teles@gmail.com")
+        insert_email = driver.find_element(by=By.XPATH, value=selecte_mail).send_keys(email)
         print("E-mail Colocado com Sucesso!")
-        time.sleep(5)
+        time.sleep(3)
 
-        insert_opt_program = driver.find_element(by=By.XPATH, value=select_program_milhas)
-        insert_opt_program.click()
+        insert_opt_program = driver.find_element(by=By.XPATH, value=select_program_milhas).click()
         print("Opções do Programa Selecionado!")
 
+        insert_select_smiles = driver.find_element(by=By.XPATH, value=select_type_program).click()
+        print("Programa Selecionado!")
+        time.sleep(3)
 
-        insert_select_smiles = driver.find_element(by=By.XPATH, value=select_type_smiles)
-        insert_select_smiles.click()
-        print("Smiles Selecionado!")
-        time.sleep(5)
-
-        insert_options_qtd = driver.find_element(by=By.XPATH, value=options_qtd_milhas)
-        insert_options_qtd.click()
+        insert_options_qtd = driver.find_element(by=By.XPATH, value=options_qtd_milhas).click()
         print("Opções de Quantidade Selecionada!")
+        time.sleep(3)
 
+        insert_select_qtd_select = driver.find_element(by=By.XPATH, value=select_qtd_milhas).click()
+        print("Milhas Selecionada!")
 
-        insert_select_qtd_select = driver.find_element(by=By.XPATH, value=select_qtd_milhas)
-        insert_select_qtd_select.click()
-        print("100mil Milhas Selecionada!")
-
-
-        insert_btn_cotar = driver.find_element(by=By.XPATH, value=btn_cotar)
-        insert_btn_cotar.click()
+        insert_btn_cotar = driver.find_element(by=By.XPATH, value=btn_cotar).click()
+        print("Cotacao Realizada com Sucesso!")
     except:
         print("Nao foi possivel realizar Sua Cotação!")
-    
+
     driver.close()
-    
+
+#Caso nao queria execultar com o agendamento descomente a linha 74 e comente ou apague as linhas 76 a 88
+#price()
 
 # Tipos de Agendamentos
-# schedule.every(3).seconds.do(price)
+# Por segundo, minutos ou hora.
+#schedule.every(1).seconds.do(price)
 schedule.every(1).minutes.do(price)
-# schedule.every(5).hour.do(price)
+# schedule.every(5).hours.do(price)
 
 while True:
     schedule.run_pending()
-    print("Bot Trabalhando...")
-    time.sleep(1)
+    data_e_hora = datetime.now()
+    hora = data_e_hora.strftime('%d/%m/%Y %H:%M')
+    # enquanto o Bot estiver rodando ele vai esta printando essa mensagem a cada x segundos que voce quiser.
+    print(hora,"- Bot Trabalhando...")
+    time.sleep(10)
 
 
 
